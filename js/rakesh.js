88df51f246564e1361af91807c99bb833b291db4
Rakesh Sharma <rasharma@cimpress.com>
2017-08-03 17:40:14 +0530

    now need to add your content

---
+++ js/rakesh.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  var selector = '.nav li';
+
+  $(selector).on('click', function () {
+    $(selector).removeClass('active');
+    $(this).addClass('active');
+  });
+});
